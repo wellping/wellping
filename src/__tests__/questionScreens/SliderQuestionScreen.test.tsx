522a11f24833f927c1cae92f3444ce29f9bb5761
Yifei He <yifei@hesyifei.com>
2020-06-21 10:35:41 -0700

    `Slider` with constant default value & update values

--- src/__tests__/questionScreens/SliderQuestionScreen.test.tsx
+++ src/__tests__/questionScreens/SliderQuestionScreen.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "react-native-testing-library";
+import { render, fireEvent, A11yAPI } from "react-native-testing-library";
 import { ReactTestInstance } from "react-test-renderer";
 
 import { SliderAnswerData } from "../../helpers/answerTypes";
@@ -9,8 +9,6 @@ import SliderQuestionScreen, {
   DEFAULT_SLIDER_VALUE,
 } from "../../questionScreens/SliderQuestionScreen";
 
-const SLIDER_A11Y_LABEL = "slider input";
-
 const SLIDER_QUESTIONS: {
   [questionId: string]: SliderQuestion;
 } = {
@@ -19,10 +17,10 @@ const SLIDER_QUESTIONS: {
     type: QuestionType.Slider,
     question: "This question does not have any default value settings.",
     slider: ["leftmost", "rightmost"],
-    next: "WithDefault",
+    next: "WithConstantDefault",
   },
-  WithDefault: {
-    id: "WithDefault",
+  WithConstantDefault: {
+    id: "WithConstantDefault",
     type: QuestionType.Slider,
     question: "This question has a default value.",
     defaultValue: 25,
@@ -30,9 +28,16 @@ const SLIDER_QUESTIONS: {
     next: null,
   },
 };
-
 const WITHOUT_DEFAULT = SLIDER_QUESTIONS["WithoutDefault"];
+const WITH_CONSTANT_DEFAULT = SLIDER_QUESTIONS["WithConstantDefault"];
 
+const SLIDER_A11Y_LABEL = "slider input";
+const getSlider = (getAllByA11yLabel: A11yAPI["getAllByA11yLabel"]) => {
+  const sliderInputs = getAllByA11yLabel(SLIDER_A11Y_LABEL);
+  expect(sliderInputs).toHaveLength(1);
+  const sliderInput = sliderInputs[0];
+  return sliderInput;
+};
 const moveSlider = (sliderInput: ReactTestInstance, value: number) => {
   fireEvent(sliderInput, "onSlidingComplete", {
     nativeEvent: { value },
@@ -62,17 +67,67 @@ test("without default value", () => {
   // Because there isn't any requirement to validate the data.
   expect(mockSetDataValidationFunction).not.toHaveBeenCalled();
 
-  const sliderInputs = getAllByA11yLabel(SLIDER_A11Y_LABEL);
-  expect(sliderInputs).toHaveLength(1);
-  const sliderInput = sliderInputs[0];
-
-  expect(toJSON()).toMatchSnapshot();
-
+  const sliderInput = getSlider(getAllByA11yLabel);
   expect(sliderInput.props.value).toBe(DEFAULT_SLIDER_VALUE);
 
-  const NEW_VALUE = 75;
-  moveSlider(sliderInput, NEW_VALUE);
-  expect(mockOnDataChangeFn).toHaveBeenCalledWith({
-    value: NEW_VALUE,
-  } as SliderAnswerData);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test("with constant default value", () => {
+  const mockOnDataChangeFn = jest.fn();
+  const mockPipeInExtraMetaData = jest.fn();
+  const mockSetDataValidationFunction = jest.fn();
+
+  const { getAllByA11yLabel, toJSON } = render(
+    <SliderQuestionScreen
+      key={WITH_CONSTANT_DEFAULT.id}
+      question={WITH_CONSTANT_DEFAULT}
+      onDataChange={mockOnDataChangeFn}
+      allAnswers={{}}
+      allQuestions={SLIDER_QUESTIONS}
+      pipeInExtraMetaData={mockPipeInExtraMetaData}
+      setDataValidationFunction={mockSetDataValidationFunction}
+    />,
+  );
+
+  // Because there isn't a need to pipe in any data.
+  expect(mockPipeInExtraMetaData).not.toHaveBeenCalled();
+
+  // Because there isn't any requirement to validate the data.
+  expect(mockSetDataValidationFunction).not.toHaveBeenCalled();
+
+  const sliderInput = getSlider(getAllByA11yLabel);
+  expect(sliderInput.props.value).toBe(WITH_CONSTANT_DEFAULT.defaultValue);
+
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test("update values", () => {
+  const mockOnDataChangeFn = jest.fn();
+  const mockPipeInExtraMetaData = jest.fn();
+  const mockSetDataValidationFunction = jest.fn();
+
+  const { getAllByA11yLabel } = render(
+    <SliderQuestionScreen
+      key={WITHOUT_DEFAULT.id}
+      question={WITHOUT_DEFAULT}
+      onDataChange={mockOnDataChangeFn}
+      allAnswers={{}}
+      allQuestions={SLIDER_QUESTIONS}
+      pipeInExtraMetaData={mockPipeInExtraMetaData}
+      setDataValidationFunction={mockSetDataValidationFunction}
+    />,
+  );
+
+  const sliderInput = getSlider(getAllByA11yLabel);
+
+  const NEW_VALUES = [88, 99, 66, 55];
+  for (let i = 0; i < NEW_VALUES.length; i++) {
+    const NEW_VALUE = NEW_VALUES[i];
+    moveSlider(sliderInput, NEW_VALUE);
+    expect(mockOnDataChangeFn).toHaveBeenNthCalledWith(i + 1, {
+      value: NEW_VALUE,
+    } as SliderAnswerData);
+    expect(mockOnDataChangeFn).toHaveBeenCalledTimes(i + 1);
+  }
 });
