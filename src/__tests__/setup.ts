bc6db20d1382393f4c2df10678e9fe9af381da96
Yifei He <yifei@hesyifei.com>
2020-06-21 14:02:51 -0700

    Minor changes in setup

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,12 +1,12 @@
-beforeEach(() => {
-  const originalErrorFn = console.error;
-  jest.spyOn(console, "error").mockImplementation((error: string) => {
-    if (
-      error.includes("Warning: Slider has been extracted from react-native")
-    ) {
-      // Slience error about Slider.
-      return;
-    }
-    originalErrorFn(error);
-  });
+const originalErrorFn = console.error;
+jest.spyOn(console, "error").mockImplementation((...error: any[]) => {
+  if (
+    (error[0] as string).includes(
+      "Warning: Slider has been extracted from react-native",
+    )
+  ) {
+    // Slience error about Slider.
+    return;
+  }
+  originalErrorFn(...error);
 });
