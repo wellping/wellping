4e1d19c737a9f35412a13ab1cf34bd7b92fef2d7
Yifei He <yifei@hesyifei.com>
2020-07-30 14:11:21 +0800

    Set __DEV__ to false in jest

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -26,6 +26,10 @@ beforeEach(() => {
 });
 
 beforeAll(() => {
+  // Similar to https://github.com/expo/expo/blob/dba287e9686718ff6375f9274d0e5c72b6c3ff4e/packages/expo-asset/src/__tests__/Asset-test.ts#L213-L220
+  // @ts-ignore: the type declaration marks __DEV__ as read-only
+  __DEV__ = false; // eslint-disable-line no-global-assign
+
   /** TEST TEST DATA INTEGRITY **/
   (() => {
     let lastPingTime = new Date(0);
@@ -46,4 +50,10 @@ beforeAll(() => {
   test("Timezone should always be UTC", () => {
     expect(new Date().getTimezoneOffset()).toBe(0);
   });
+});
+
+afterAll(() => {
+  // Similar to https://github.com/expo/expo/blob/dba287e9686718ff6375f9274d0e5c72b6c3ff4e/packages/expo-asset/src/__tests__/Asset-test.ts#L213-L220
+  // @ts-ignore: the type declaration marks __DEV__ as read-only
+  __DEV__ = true; // eslint-disable-line no-global-assign
 });
