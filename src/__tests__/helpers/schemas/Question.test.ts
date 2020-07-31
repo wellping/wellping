6d69458740f369f2895161d949b83e6e388b8787
Yifei He <yifei@hesyifei.com>
2020-07-31 11:29:34 +0800

    Ad tests for SliderQuestionSchema

--- src/__tests__/helpers/schemas/Question.test.ts
+++ src/__tests__/helpers/schemas/Question.test.ts
@@ -1,4 +1,8 @@
-import { QuestionSchema } from "../../../helpers/schemas/Question";
+import { QuestionType } from "../../../helpers/helpers";
+import {
+  QuestionSchema,
+  SliderQuestionSchema,
+} from "../../../helpers/schemas/Question";
 
 describe("QuestionSchema", () => {
   test("empty", () => {
@@ -11,7 +15,7 @@ describe("QuestionSchema", () => {
     expect(() => {
       QuestionSchema.parse({
         id: "Feel_Ideal",
-        type: "Slider",
+        type: QuestionType.Slider,
         question: "(This is missing `slider` field)",
         defaultValueFromQuestionId: "Feel_Current",
         next: "Stressor",
@@ -23,7 +27,7 @@ describe("QuestionSchema", () => {
     expect(() => {
       QuestionSchema.parse({
         id: "Feel_Ideal",
-        type: "Slider",
+        type: QuestionType.Slider,
         question: "(This is missing `next` field)",
         defaultValueFromQuestionId: "Feel_Current",
         slider: ["left", "right"],
@@ -33,7 +37,7 @@ describe("QuestionSchema", () => {
     expect(() => {
       QuestionSchema.parse({
         id: "Feel_Ideal",
-        type: "Slider",
+        type: QuestionType.Slider,
         question: "Good question",
         defaultValueFromQuestionId: "Feel_Current",
         slider: ["left", "right"],
@@ -44,7 +48,7 @@ describe("QuestionSchema", () => {
     expect(() => {
       QuestionSchema.parse({
         id: "Feel_Ideal",
-        type: "Slider",
+        type: QuestionType.Slider,
         question: "Good question",
         defaultValueFromQuestionId: "Feel_Current",
         slider: ["left", "right"],
@@ -80,12 +84,162 @@ describe("QuestionSchema", () => {
     expect(() => {
       QuestionSchema.parse({
         id: "Feel_Ideal",
-        type: "ChoicesWithSingleAnswer",
+        type: QuestionType.ChoicesWithSingleAnswer,
         question: "Bad type question",
         defaultValueFromQuestionId: "Feel_Current",
         slider: ["left", "right"],
         next: "Next_Question",
       });
     }).toThrowErrorMatchingSnapshot();
+  });
+});
+
+describe("SliderQuestionSchema", () => {
+  test("type", () => {
+    const question = {
+      id: "Feel_Ideal",
+      question: "Slider question",
+      next: "Next_Question",
+      slider: ["left", "right"],
+    };
+
+    expect(() => {
+      SliderQuestionSchema.parse({
+        ...question,
+        type: QuestionType.Slider,
+      });
+    }).not.toThrowError();
+
+    expect(() => {
+      SliderQuestionSchema.parse({
+        ...question,
+        type: QuestionType.HowLongAgo,
+      });
+    }).toThrowErrorMatchingSnapshot();
+  });
+
+  test("slider tuple", () => {
+    const question = {
+      id: "Feel_Ideal",
+      type: QuestionType.Slider,
+      question: "Slider question",
+      next: "Next_Question",
+    };
+
+    expect(() => {
+      SliderQuestionSchema.parse({
+        ...question,
+        slider: ["left", "right"],
+      });
+    }).not.toThrowError();
+
+    expect(() => {
+      SliderQuestionSchema.parse({
+        ...question,
+        slider: ["left", "right", "top"],
+      });
+    }).toThrowErrorMatchingSnapshot("three items");
+
+    expect(() => {
+      SliderQuestionSchema.parse({
+        ...question,
+        slider: ["left"],
+      });
+    }).toThrowErrorMatchingSnapshot("one item");
+
+    expect(() => {
+      SliderQuestionSchema.parse({
+        ...question,
+        slider: [],
+      });
+    }).toThrowErrorMatchingSnapshot("zero item");
+  });
+
+  test("default value range 0 - 100", () => {
+    const question = {
+      id: "Feel_Ideal",
+      type: QuestionType.Slider,
+      question: "Range question",
+      slider: ["left", "right"],
+      next: "Next_Question",
+    };
+
+    expect(() => {
+      SliderQuestionSchema.parse({
+        ...question,
+        defaultValue: 50,
+      });
+    }).not.toThrowError();
+
+    expect(() => {
+      SliderQuestionSchema.parse({
+        ...question,
+        defaultValue: 0,
+      });
+    }).not.toThrowError();
+
+    expect(() => {
+      SliderQuestionSchema.parse({
+        ...question,
+        defaultValue: 100,
+      });
+    }).not.toThrowError();
+
+    expect(() => {
+      SliderQuestionSchema.parse({
+        ...question,
+        defaultValue: -1,
+      });
+    }).toThrowErrorMatchingSnapshot("<0");
+
+    expect(() => {
+      SliderQuestionSchema.parse({
+        ...question,
+        defaultValue: 101,
+      });
+    }).toThrowErrorMatchingSnapshot(">100");
+  });
+
+  describe("defaultValueFromQuestionId", () => {
+    const question = {
+      id: "Feel_Ideal",
+      type: QuestionType.Slider,
+      question: "Range question",
+      slider: ["left", "right"],
+      next: "Next_Question",
+    };
+
+    test("can be undefined", () => {
+      expect(() => {
+        SliderQuestionSchema.parse({
+          ...question,
+        });
+      }).not.toThrowError();
+    });
+
+    test("cannot be null", () => {
+      expect(() => {
+        SliderQuestionSchema.parse({
+          ...question,
+          defaultValueFromQuestionId: null,
+        });
+      }).toThrowErrorMatchingSnapshot();
+    });
+
+    test("is question ID", () => {
+      expect(() => {
+        SliderQuestionSchema.parse({
+          ...question,
+          defaultValueFromQuestionId: "valid_question_id",
+        });
+      }).not.toThrowError();
+
+      expect(() => {
+        SliderQuestionSchema.parse({
+          ...question,
+          defaultValueFromQuestionId: "INVALID QUESTION-ID",
+        });
+      }).toThrowErrorMatchingSnapshot();
+    });
   });
 });
