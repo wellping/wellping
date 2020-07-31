b268bead77431823379d1151b6c99878feb81d4b
Yifei He <yifei@hesyifei.com>
2020-07-31 10:00:48 +0800

    Add test for QuestionIdSchema

--- src/helpers/schemas/common.ts
+++ src/helpers/schemas/common.ts
@@ -6,14 +6,23 @@ import * as z from "zod";
 
 import { idRegexCheck, idRegexErrorMessage } from "./helper";
 
-export const StudyIdSchema = z.string().refine(idRegexCheck, {
-  message: idRegexErrorMessage("Study ID"),
-});
+export const StudyIdSchema = z
+  .string()
+  .nonempty()
+  .refine(idRegexCheck, {
+    message: idRegexErrorMessage("Study ID"),
+  });
 
-export const StreamNameSchema = z.string().refine(idRegexCheck, {
-  message: idRegexErrorMessage("Stream name"),
-});
+export const StreamNameSchema = z
+  .string()
+  .nonempty()
+  .refine(idRegexCheck, {
+    message: idRegexErrorMessage("Stream name"),
+  });
 
-export const QuestionIdSchema = z.string().refine(idRegexCheck, {
-  message: idRegexErrorMessage("Question ID"),
-});
+export const QuestionIdSchema = z
+  .string()
+  .nonempty()
+  .refine(idRegexCheck, {
+    message: idRegexErrorMessage("Question ID"),
+  });
