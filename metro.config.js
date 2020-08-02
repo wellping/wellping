4831ddc899118366a9a73f02c87d0dbdce5ed5a1
Yifei He <yifei@hesyifei.com>
2020-08-02 15:36:25 +0800

    Fix typeorm error in production mode

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   resolver: {
-    sourceExts: ['js', 'json', 'ts', 'tsx', 'jsx']
-  }
+    sourceExts: ["js", "json", "ts", "tsx", "jsx"],
+  },
+  transformer: {
+    // https://github.com/typeorm/typeorm/issues/4723#issuecomment-631116050
+    // https://forums.expo.io/t/change-minifierconfig-for-minify-uglify/36460/2
+    minifierConfig: {
+      keep_classnames: true, // FIX typeorm
+      keep_fnames: true, // FIX typeorm
+      mangle: {
+        keep_classnames: true, // FIX typeorm
+        keep_fnames: true, // FIX typeorm
+      },
+    },
+  },
 };
