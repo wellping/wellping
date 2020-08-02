8b570b34c187a02478961d10d5bee6547dd05086
Yifei He <yifei@hesyifei.com>
2020-08-02 15:42:25 +0800

    Add `JS_VERSION_NUMBER` to loading screen

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import { JS_VERSION_NUMBER } from "../helpers/debug";
 import { styles } from "../helpers/styles";
 
-export const Loading: React.FunctionComponent = ({ children }) => {
+export const Loading: React.FunctionComponent = () => {
   return (
     <View>
       <Text style={styles.onlyTextStyle}>Loading...</Text>
+      <Text style={{ textAlign: "center", marginTop: 10, color: "lightgray" }}>
+        v.{JS_VERSION_NUMBER}
+      </Text>
     </View>
   );
 };
