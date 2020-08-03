9f5d950b76397f1447970abdd82c494800347681
Yifei He <yifei@hesyifei.com>
2020-08-03 11:59:58 +0800

    Add a notice in Loading page

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,14 +1,34 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-import { JS_VERSION_NUMBER } from "../helpers/debug";
+import { JS_VERSION_NUMBER, shareDebugText } from "../helpers/debug";
 import { styles } from "../helpers/styles";
 
 export const Loading: React.FunctionComponent = () => {
   return (
     <View>
-      <Text style={styles.onlyTextStyle}>Loading...</Text>
-      <Text style={{ textAlign: "center", marginTop: 10, color: "lightgray" }}>
+      <Text style={{ ...styles.onlyTextStyle, fontWeight: "bold" }}>
+        Loading...
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          marginHorizontal: 20,
+          marginTop: 50,
+          color: "lightgray",
+        }}
+      >
+        If you are stuck on this page, please click the button below and contact
+        the research staff.
+      </Text>
+      <Button
+        title="Share Error"
+        color="lightgray"
+        onPress={() => {
+          shareDebugText("Stuck on the loading page.");
+        }}
+      />
+      <Text style={{ textAlign: "center", marginTop: 50, color: "lightgray" }}>
         v.{JS_VERSION_NUMBER}
       </Text>
     </View>
