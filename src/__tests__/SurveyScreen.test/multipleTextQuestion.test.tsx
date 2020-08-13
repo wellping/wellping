0cc7f4ee13833a8fed767bb9cf18940969879386
Yifei He <yifei@hesyifei.com>
2020-08-13 08:49:36 +0800

    with repeatedItemStartId test

--- src/__tests__/SurveyScreen.test/multipleTextQuestion.test.tsx
+++ src/__tests__/SurveyScreen.test/multipleTextQuestion.test.tsx
@@ -89,6 +89,261 @@ describe("without repeatedItemStartId", () => {
     });
   });
 
+  test("enter some text and then remove them", async () => {
+    const onFinishFn = jest.fn();
+
+    const renderResults = render(
+      <SurveyScreen {...props} onFinish={onFinishFn} />,
+    );
+
+    await testQuestionsSequenceAsync({
+      renderResults,
+      onFinishFn,
+      sequence: [
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            await enterTextAsync(0, "hello world", renderResults);
+            await enterTextAsync(1, "yes?", renderResults);
+            await enterTextAsync(0, "", renderResults);
+            await enterTextAsync(1, "", renderResults);
+
+            expect(getCurrentQuestionTitle()).toBe("Question 1");
+          },
+        },
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            expect(getCurrentQuestionTitle()).toBe("Question 2");
+          },
+        },
+      ],
+    });
+  });
+
+  test("click next without answering", async () => {
+    const onFinishFn = jest.fn();
+
+    const renderResults = render(
+      <SurveyScreen {...props} onFinish={onFinishFn} />,
+    );
+
+    await testQuestionsSequenceAsync({
+      renderResults,
+      onFinishFn,
+      sequence: [
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            expect(getCurrentQuestionTitle()).toBe("Question 1");
+          },
+        },
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            expect(getCurrentQuestionTitle()).toBe("Question 2");
+          },
+        },
+      ],
+    });
+  });
+
+  test("click prefer not to answer", async () => {
+    const onFinishFn = jest.fn();
+
+    const renderResults = render(
+      <SurveyScreen {...props} onFinish={onFinishFn} />,
+    );
+
+    await testQuestionsSequenceAsync({
+      renderResults,
+      onFinishFn,
+      sequence: [
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            // Entering a text shouldn't matter here if we click prefer not
+            // to answer.
+            await enterTextAsync(0, "hello!", renderResults);
+
+            expect(getCurrentQuestionTitle()).toBe("Question 1");
+          },
+          nextButton: "pna",
+        },
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            expect(getCurrentQuestionTitle()).toBe("Question 2");
+          },
+        },
+      ],
+    });
+  });
+});
+
+describe("with repeatedItemStartId", () => {
+  const props: SurveyScreenProps = {
+    ...propsBase,
+    questions: {
+      q1: {
+        id: "q1",
+        type: QuestionType.MultipleText,
+        question: "Question 1",
+        indexName: "INDEX",
+        variableName: "MY_VARNAME",
+        repeatedItemStartId: "q1_n[__INDEX__]_1",
+        max: 3,
+        next: "q2",
+      },
+      "q1_n[__INDEX__]_1": {
+        id: "q1_n[__INDEX__]_1",
+        type: QuestionType.Slider,
+        question: "Question 1-1: [__MY_VARNAME__] (index [__INDEX__])",
+        slider: ["left", "right"],
+        next: "q1_n[__INDEX__]_2",
+      },
+      "q1_n[__INDEX__]_2": {
+        id: "q1_n[__INDEX__]_2",
+        type: QuestionType.Slider,
+        question: "Question 1-2: [__MY_VARNAME__] (index is [__INDEX__])",
+        slider: ["left", "right"],
+        next: null,
+      },
+      q2: {
+        id: "q2",
+        type: QuestionType.Slider,
+        question: "Question 2",
+        slider: ["left", "right"],
+        next: null,
+      },
+    },
+  };
+
+  test("enter some text", async () => {
+    const onFinishFn = jest.fn();
+
+    const renderResults = render(
+      <SurveyScreen {...props} onFinish={onFinishFn} />,
+    );
+
+    await testQuestionsSequenceAsync({
+      renderResults,
+      onFinishFn,
+      sequence: [
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            await enterTextAsync(0, "hello world!", renderResults);
+            await enterTextAsync(1, "yes?", renderResults);
+
+            expect(getCurrentQuestionTitle()).toBe("Question 1");
+          },
+        },
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            expect(getCurrentQuestionTitle()).toBe(
+              "Question 1-1: hello world! (index 1)",
+            );
+          },
+        },
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            expect(getCurrentQuestionTitle()).toBe(
+              "Question 1-2: hello world! (index is 1)",
+            );
+          },
+        },
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            expect(getCurrentQuestionTitle()).toBe(
+              "Question 1-1: yes? (index 2)",
+            );
+          },
+        },
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            expect(getCurrentQuestionTitle()).toBe(
+              "Question 1-2: yes? (index is 2)",
+            );
+          },
+        },
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            expect(getCurrentQuestionTitle()).toBe("Question 2");
+          },
+        },
+      ],
+    });
+  });
+
+  test("enter some text and then remove them", async () => {
+    const onFinishFn = jest.fn();
+
+    const renderResults = render(
+      <SurveyScreen {...props} onFinish={onFinishFn} />,
+    );
+
+    await testQuestionsSequenceAsync({
+      renderResults,
+      onFinishFn,
+      sequence: [
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            await enterTextAsync(0, "hello world", renderResults);
+            await enterTextAsync(1, "yes?", renderResults);
+            await enterTextAsync(0, "", renderResults);
+            await enterTextAsync(1, "", renderResults);
+
+            expect(getCurrentQuestionTitle()).toBe("Question 1");
+          },
+        },
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            expect(getCurrentQuestionTitle()).toBe("Question 2");
+          },
+        },
+      ],
+    });
+  });
+
+  test("enter some text and then remove one of them", async () => {
+    const onFinishFn = jest.fn();
+
+    const renderResults = render(
+      <SurveyScreen {...props} onFinish={onFinishFn} />,
+    );
+
+    await testQuestionsSequenceAsync({
+      renderResults,
+      onFinishFn,
+      sequence: [
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            await enterTextAsync(0, "後之視今", renderResults);
+            await enterTextAsync(1, "亦猶今之視昔", renderResults);
+            await enterTextAsync(2, "悲夫", renderResults);
+            await enterTextAsync(0, "", renderResults);
+            await enterTextAsync(1, "", renderResults);
+
+            expect(getCurrentQuestionTitle()).toBe("Question 1");
+          },
+        },
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            expect(getCurrentQuestionTitle()).toBe(
+              "Question 1-1: 悲夫 (index 1)",
+            );
+          },
+        },
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            expect(getCurrentQuestionTitle()).toBe(
+              "Question 1-2: 悲夫 (index is 1)",
+            );
+          },
+        },
+        {
+          expectCurrentQuestionAsync: async (getCurrentQuestionTitle) => {
+            expect(getCurrentQuestionTitle()).toBe("Question 2");
+          },
+        },
+      ],
+    });
+  });
+
   test("click next without answering", async () => {
     const onFinishFn = jest.fn();
 
