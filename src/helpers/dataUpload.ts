e2a47cb8ae6ff5ca2c52669e443f34fa0a26f84e
Yifei He <yifei@hesyifei.com>
2020-08-17 15:49:32 +0800

    Upload device and app information as well

--- src/helpers/dataUpload.ts
+++ src/helpers/dataUpload.ts
@@ -1,13 +1,39 @@
+import Constants from "expo-constants";
+import * as Device from "expo-device";
+
 import { AnswerEntity } from "../entities/AnswerEntity";
 import { PingEntity } from "../entities/PingEntity";
 import { getAnswersAsync } from "./answers";
 import { getUserAsync } from "./asyncStorage/user";
-import { HOME_SCREEN_DEBUG_VIEW_SYMBOLS } from "./debug";
+import {
+  HOME_SCREEN_DEBUG_VIEW_SYMBOLS,
+  JS_VERSION_NUMBER,
+  NATIVE_VERSION_NUMBER,
+  NATIVE_BUILD_NUMBER,
+} from "./debug";
 import { firebaseUploadDataForUserAsync } from "./firebase";
 import { getPingsAsync } from "./pings";
 
 export type UploadData = {
-  username: string;
+  user: {
+    username: string;
+    device: {
+      brand: typeof Device.brand;
+      manufacturer: typeof Device.manufacturer;
+      modelName: typeof Device.modelName;
+      modelId: typeof Device.modelId;
+      designName: typeof Device.designName;
+      productName: typeof Device.productName;
+      osName: typeof Device.osName;
+      osVersion: typeof Device.osVersion;
+    };
+    app: {
+      jsVersion: typeof JS_VERSION_NUMBER;
+      nativeVersion: typeof NATIVE_VERSION_NUMBER;
+      nativeBuild: typeof NATIVE_BUILD_NUMBER;
+      installationId: typeof Constants.installationId;
+    };
+  };
   pings: PingEntity[];
   answers: AnswerEntity[];
 };
@@ -17,8 +43,30 @@ export async function getAllDataAsync(): Promise<UploadData> {
   const pings = await getPingsAsync();
   const answers = await getAnswersAsync();
 
-  const data = {
-    username: user!.username,
+  if (user === null) {
+    throw new Error("user === null in getAllDataAsync");
+  }
+
+  const data: UploadData = {
+    user: {
+      username: user.username,
+      device: {
+        brand: Device.brand,
+        manufacturer: Device.manufacturer,
+        modelName: Device.modelName,
+        modelId: Device.modelId,
+        designName: Device.designName,
+        productName: Device.productName,
+        osName: Device.osName,
+        osVersion: Device.osVersion,
+      },
+      app: {
+        jsVersion: JS_VERSION_NUMBER,
+        nativeVersion: NATIVE_VERSION_NUMBER,
+        nativeBuild: NATIVE_BUILD_NUMBER,
+        installationId: Constants.installationId,
+      },
+    },
     pings,
     answers,
   };
