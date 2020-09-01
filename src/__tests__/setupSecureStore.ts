e494c6a4154fe713039ae55b1db344830dcc6f74
Yifei He <yifei@hesyifei.com>
2020-09-01 10:07:24 +0800

    Follow the same usage from setupAsyncStorage for setupSecureStore

--- src/__tests__/setupSecureStore.ts
+++ src/__tests__/setupSecureStore.ts
@@ -5,30 +5,36 @@
 // https://stackoverflow.com/a/50080250/2603230
 // https://stackoverflow.com/a/41469576/2603230
 
-const mockSecureItems: { [key: string]: any } = {};
-jest.mock("expo-secure-store", () => ({
+// Most code is structured from
+// https://github.com/react-native-community/async-storage/blob/b1b5bcf9a1aa4c55590e731535c0af22bd2e71d6/jest/async-storage-mock.js
+
+const mockSecureStore: any = {
+  __INTERNAL_MOCK_STORAGE__: {} as { [key: string]: any },
+
   setItemAsync: jest.fn((key, value) => {
     //console.log(`expo-secure-store setItemAsync(${key}, ${value})`);
     return new Promise((resolve, reject) => {
       return typeof key !== "string" || typeof value !== "string"
         ? reject(new Error("key and value must be string"))
-        : resolve((mockSecureItems[key] = value));
+        : resolve((mockSecureStore.__INTERNAL_MOCK_STORAGE__[key] = value));
     });
   }),
   getItemAsync: jest.fn((key) => {
     //console.log(`expo-secure-store getItemAsync(${key})`);
     return new Promise((resolve, reject) => {
-      return mockSecureItems.hasOwnProperty(key)
-        ? resolve(mockSecureItems[key])
+      return mockSecureStore.__INTERNAL_MOCK_STORAGE__.hasOwnProperty(key)
+        ? resolve(mockSecureStore.__INTERNAL_MOCK_STORAGE__[key])
         : resolve(null);
     });
   }),
   deleteItemAsync: jest.fn((key) => {
     //console.log(`expo-secure-store deleteItemAsync(${key})`);
     return new Promise((resolve, reject) => {
-      return mockSecureItems.hasOwnProperty(key)
-        ? resolve(delete mockSecureItems[key])
+      return mockSecureStore.__INTERNAL_MOCK_STORAGE__.hasOwnProperty(key)
+        ? resolve(delete mockSecureStore.__INTERNAL_MOCK_STORAGE__[key])
         : reject(new Error("No such key!"));
     });
   }),
-}));
+};
+
+jest.mock("expo-secure-store", () => mockSecureStore);
