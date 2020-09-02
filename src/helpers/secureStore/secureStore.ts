7eb2c26d5584e3b6cf77ee49df1ad50f32f499b2
Yifei He <yifei@hesyifei.com>
2020-09-02 14:42:30 +0800

    Add INSTALLATION_ID to SecureStore key prefix

--- src/helpers/secureStore/secureStore.ts
+++ src/helpers/secureStore/secureStore.ts
@@ -1,8 +1,12 @@
+import { INSTALLATION_ID } from "../debug";
 import { getStudyInfoAsync } from "../studyFile";
 
 export async function getSSKeyAsync(key: string = ""): Promise<string> {
   const studyInfo = await getStudyInfoAsync();
   // Keys provided to SecureStore must not be empty and contain only
   // alphanumeric characters, ".", "-", and "_".
-  return `WELLPING-Study_${studyInfo.id}.${key}`;
+  // Note: We include `INSTALLATION_ID` so that the data will NOT be accessible
+  // across different installations (as SecureStore will be accessible across
+  // different installations by default on iOS).
+  return `WELLPING-${INSTALLATION_ID}-Study_${studyInfo.id}.${key}`;
 }
