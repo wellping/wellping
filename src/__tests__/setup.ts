ad2cf1627937b83b68f0edebad26d50b9a6b1db0
Yifei He <yifei@hesyifei.com>
2021-12-12 17:04:23 -0800

    set `__DEV__` to `false` in test

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,10 +1,6 @@
 import { PINGS, PINGS_DICT } from "./data/pings";
 
 beforeAll(() => {
-  // Similar to https://github.com/expo/expo/blob/dba287e9686718ff6375f9274d0e5c72b6c3ff4e/packages/expo-asset/src/__tests__/Asset-test.ts#L213-L220
-  // @ts-ignore: the type declaration marks __DEV__ as read-only
-  __DEV__ = false; // eslint-disable-line no-global-assign
-
   /** TEST TEST DATA INTEGRITY **/
   (() => {
     let lastPingTime = new Date(0);
@@ -27,8 +23,4 @@ beforeAll(() => {
   });
 });
 
-afterAll(() => {
-  // Similar to https://github.com/expo/expo/blob/dba287e9686718ff6375f9274d0e5c72b6c3ff4e/packages/expo-asset/src/__tests__/Asset-test.ts#L213-L220
-  // @ts-ignore: the type declaration marks __DEV__ as read-only
-  __DEV__ = true; // eslint-disable-line no-global-assign
-});
+afterAll(() => {});
