1f7efa2d07f6fb3e76db5fc25ef5d6eb4cd019ab
Yifei He <yifei@hesyifei.com>
2021-12-17 11:31:55 -0800

    Update helpers.ts

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -10,6 +10,7 @@ import {
 } from "./types";
 
 export const QuestionType = QuestionTypeSchema.enum;
+export const QUESTION_TYPES = QuestionTypeSchema.options;
 
 export const NON_USER_QUESTION_TYPES: QuestionTypeType[] = [
   QuestionType.Branch,
